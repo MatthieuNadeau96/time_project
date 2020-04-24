bff183cd9cbdeb1a094f807a7f16ee35f9350a4b
Matthieu <matthieunadeau96@gmail.com>
2020-04-24 14:43:50 -0400

    Added a tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -20,12 +21,41 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  TabController tb;
+
+  @override
+  void initState() {
+    tb = TabController(
+      length: 2,
+      vsync: this,
+    );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Time Project"),
+        centerTitle: true,
+        bottom: TabBar(
+          tabs: <Widget>[
+            Text('Timer'),
+            Text('Stopwatch'),
+          ],
+          labelPadding: EdgeInsets.only(bottom: 10),
+          labelStyle: TextStyle(fontSize: 18.0),
+          unselectedLabelColor: Colors.white60,
+          controller: tb,
+        ),
+      ),
+      body: TabBarView(
+        children: <Widget>[
+          Text('Timer'),
+          Text('Stopwatch'),
+        ],
+        controller: tb,
       ),
     );
   }
